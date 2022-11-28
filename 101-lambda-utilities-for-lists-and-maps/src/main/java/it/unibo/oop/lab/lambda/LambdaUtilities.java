7efbc8be70106f1e5304ee49fe5c9efcf29d6e92
Tassinari Leonardo <leonardo.tassinari6@studio.unibo.it>
2022-11-28 15:51:44 +0100

    Es 101 done

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,8 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -61,7 +63,11 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        final List<Optional<T>> l = new LinkedList<>();
+        list.forEach(t -> {
+            l.add(Optional.ofNullable(t).filter(pre));
+        });
+        return l;
     }
 
     /**
@@ -80,7 +86,14 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R, Set<T>> m = new HashMap<>();
+        list.forEach(t -> {
+            m.merge(op.apply(t), new HashSet<T>(Set.of(t)), (o, n) -> {
+                o.addAll(n); 
+                return o;
+            });
+        });
+        return m;
     }
 
     /**
@@ -101,7 +114,11 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K, V> m = new HashMap<>();
+        map.forEach((k, v) -> 
+            m.put(k, v.orElse(def.get()))
+        );
+        return m;
     }
 
     /**
